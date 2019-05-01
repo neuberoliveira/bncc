dbd054ed358eb6bbc57f3dcf2effd5ba879547bd
Neuber Oliveira <neuber.oliveira05@gmail.com>
2019-05-01 13:30:21 -0300

    :tada:

---
+++ next.config.js
@@ -0,0 +1,6 @@
+// next.config.js
+const withTypescript = require('@zeit/next-typescript')
+const withSass = require('@zeit/next-sass')
+
+module.exports = withTypescript()
+module.exports = withSass()
