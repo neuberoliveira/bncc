d0a6cf8d658dbe38e519ed3a403045dd5589ed85
Neuber Oliveira <neuber.oliveira05@gmail.com>
2019-05-01 23:35:48 -0300

    more ignored words

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
-// next.config.js
 const withTypescript = require('@zeit/next-typescript')
 const withSass = require('@zeit/next-sass')
 
-module.exports = withTypescript()
-module.exports = withSass()
+module.exports = withTypescript(
+	withSass({
+		pageExtensions: ['jsx', 'js']
+	})
+)
---
+++ pages/index.tsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import "../styles.scss";
+import codesSource from "../codes.json";
+
+/* 
+"title": "O EU, O OUTRO E O NÓS",
+"itens": [
+	{
+		"code": "EI01EO01",
+		"range": "Bebês (zero a 1 ano e 6 meses)",
+		"description": "Perceber que suas ações têm efeitos nas outras crianças e nos adultos."
+	},
+*/
+type CodeSource = Array<Group>
+type SearchResult = Array<ItemFlat>
+interface Group {
+	title: string
+	itens: Array<Item>
+}
+interface Item {
+	code: string
+	range: string
+	description: string
+}
+interface ItemFlat extends Item{
+	groupIndex: number
+}
+
+
+const codes:CodeSource = codesSource
+const codesFlat:SearchResult = []
+
+codes.map((grp:Group, i:number)=>{
+	grp.itens.map((item:Item)=>{
+		codesFlat.push({
+			groupIndex: i,
+			...item,
+		})
+	})
+})
+
+function Example() {
+	const defaultResult:SearchResult = codesFlat // []
+	const [results, setResults] = useState(defaultResult);
+	// const [searching, setSearching] = useState(false);
+	
+	const doSearch = (term?:string) => {
+		const regex:RegExp = new RegExp(term+"", 'i')
+		setResults(codesFlat.filter((item:ItemFlat)=>regex.test(item.code)))
+	}
+	
+	return (
+		<div className="container">
+			<div className="row">
+				<div className="col-sm-12">
+					<input type="text" className="form-control" placeholder="Ex: EI01EO01" onKeyUp={(evt)=>doSearch(evt.target.value)} />
+				</div>
+			</div>
+			<br />
+			<div className="row">
+				{results.map((item:ItemFlat, i:number)=>(
+					<div className="col-sm-12 col-md-6 col-lg-3 mb-4">
+						<div key={i} className="card h-100 d-inline-block">
+							<div className="card-body">
+								<h5 className="card-title">{item.code}</h5>
+								<h6 className="card-subtitle mb-2 text-muted">{codes[item.groupIndex].title}</h6>
+								<p className="font-italic border-bottom pb-3" style={{fontSize:'13px'}}>{item.range}</p>
+								<p className="card-text">{item.description}</p>
+								
+							</div>
+						</div>
+					</div>
+				))}
+			</div>
+		</div>
+	);
+}
+  
+export default Example
